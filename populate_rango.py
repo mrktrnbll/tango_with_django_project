73373cb2f0d73c4005b9f9e589dab6a67b91fbaa
“Mark <2482252t@student.gla.ac.uk>
2023-01-23 12:32:12 +0000

    Chapter 6 -complete

--- populate_rango.py
+++ populate_rango.py
@@ -3,61 +3,63 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
+
 django.setup()
 from rango.models import Category, Page
 
+
 def populate():
-
-
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url': 'http://docs.python.org/3/tutorial/', 'views': 31},
+        {'title': 'How to Think like a Computer Scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/', 'views': 32},
+        {'title': 'Learn Python in 10 Minutes',
+         'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 64}]
 
     django_pages = [
-        {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+        {'title': 'Official Django Tutorial', 'views': 8,
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        {'title': 'Django Rocks',
+         'url': 'http://www.djangorocks.com/', 'views': 13},
+        {'title': 'How to Tango with Django',
+         'url': 'http://www.tangowithdjango.com/', 'views': 40}]
 
     other_pages = [
-        {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        {'title': 'Bottle',
+         'url': 'http://bottlepy.org/docs/dev/', 'views': 26},
+        {'title': 'Flask',
+         'url': 'http://flask.pocoo.org', 'views': 16}]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
-
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['likes'], cat_data['views'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
+
 def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
+    p.url = url
+    p.views = views
     p.save()
     return p
 
+
 def add_cat(name, likes=0, views=0):
     c = Category.objects.get_or_create(name=name)[0]
-    c.likes=likes
-    c.views=views
+    c.likes = likes
+    c.views = views
     c.save()
     return c
+
 
 if __name__ == '__main__':
     print('Starting Rango population script...')
--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
@@ -6,7 +7,11 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -23,3 +28,6 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_views(self):
+        return self.views
